b187094e651a61ec88fde1d2f65aec0dc63bc8f7
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-30 08:28:16 +0400

    list of @Value

--- src/main/java/com/mcs/lvl/three/configprojectdemo/controller/GreetingController.java
+++ src/main/java/com/mcs/lvl/three/configprojectdemo/controller/GreetingController.java
@@ -1,17 +1,25 @@
 package com.mcs.lvl.three.configprojectdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 public class GreetingController {
 
     @Value("${my.greeting: default hello}")
     private String greetingMessage;
 
+    @Value("${my.list.values}")
+    private List<String> values;
+
     @GetMapping("/greeting")
     public String getGreeting() {
+        values.forEach(log::info);
         return greetingMessage;
     }
 
