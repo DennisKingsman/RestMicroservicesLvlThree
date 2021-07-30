90c041bf676d334500a8a93d12eb420a7ae24566
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-30 06:23:30 +0400

    init

---
+++ src/main/java/com/mcs/lvl/three/configprojectdemo/ConfigProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mcs.lvl.three.configprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigProjectDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcs/lvl/three/configprojectdemo/ConfigProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcs.lvl.three.configprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
