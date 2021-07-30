c2a2393e96f50bf7b29749e944ed0ea17399cd69
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-30 08:42:11 +0400

    map of @Value

--- src/main/java/com/mcs/lvl/three/configprojectdemo/controller/GreetingController.java
+++ src/main/java/com/mcs/lvl/three/configprojectdemo/controller/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -17,8 +18,16 @@ public class GreetingController {
     @Value("${my.list.values}")
     private List<String> values;
 
+    @Value("#{${my.example.dbValues}}")
+    private Map<String, String> dbValues;
+
     @GetMapping("/greeting")
     public String getGreeting() {
+        log.info("Map keys");
+        dbValues.keySet().forEach(log::info);
+        log.info("Map values");
+        dbValues.values().forEach(log::info);
+        log.info("List");
         values.forEach(log::info);
         return greetingMessage;
     }
