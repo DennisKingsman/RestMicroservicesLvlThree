e908883f3c721775c0fd484ee974595f20cb6d04
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-30 08:54:51 +0400

    use ConfigurationProperties

--- src/main/java/com/mcs/lvl/three/configprojectdemo/controller/GreetingController.java
+++ src/main/java/com/mcs/lvl/three/configprojectdemo/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.mcs.lvl.three.configprojectdemo.controller;
 
+import com.mcs.lvl.three.configprojectdemo.config.DbSettingsExample;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,13 @@ public class GreetingController {
     @Value("#{${my.example.dbValues}}")
     private Map<String, String> dbValues;
 
+    private DbSettingsExample dbSettings;
+
+    @Autowired
+    public void setDbSettings(DbSettingsExample dbSettings) {
+        this.dbSettings = dbSettings;
+    }
+
     @GetMapping("/greeting")
     public String getGreeting() {
         log.info("Map keys");
@@ -29,6 +38,8 @@ public class GreetingController {
         dbValues.values().forEach(log::info);
         log.info("List");
         values.forEach(log::info);
+        log.info("DbSettings");
+        log.info(dbSettings.toString());
         return greetingMessage;
     }
 
