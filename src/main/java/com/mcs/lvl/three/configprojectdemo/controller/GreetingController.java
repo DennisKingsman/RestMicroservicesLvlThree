1911c8de8b2c7a5d35b21045a075fa2569e4e359
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-31 19:02:10 +0400

    add @RefreshScope

--- src/main/java/com/mcs/lvl/three/configprojectdemo/controller/GreetingController.java
+++ src/main/java/com/mcs/lvl/three/configprojectdemo/controller/GreetingController.java
@@ -4,6 +4,7 @@ import com.mcs.lvl.three.configprojectdemo.config.DbSettingsExample;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@RefreshScope
 public class GreetingController {
 
     @Value("${my.greeting: default hello}")
